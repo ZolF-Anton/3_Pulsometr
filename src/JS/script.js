11b1a3826f3421f909a58b497b2ed67ef65ba6fb
ZolF-werk <zolf@bk.ru>
2020-12-28 17:35:10 +0300

    carusel_rdy

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,9 +1,25 @@
 $(document).ready(function () {
     $('.carousel__inner').slick({
-        dots: true,
         infinite: true,
         speed: 1000,
-        slidesToShow: 1,
-        adaptiveHeight: true,
+        // adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow:
+            '<button type="button" class="slick-prev"><img src="icon/red/chevron_left_solid.png" /></button>',
+        nextArrow:
+            '<button type="button" class="slick-next"><img src="icon/red/chevron_right_solid.png" /></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    infinite: true,
+                    dots: true,
+                    arrows: false,
+                },
+            },
+        ],
     });
 });
